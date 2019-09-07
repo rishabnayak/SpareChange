b7b65f84c8b94e62c71994ac707d2e6243040053
rishab2113 <rishab@bu.edu>
2019-09-07 15:55:42 -0400

    updates

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sparechange/constants/colors.dart';
 import 'package:sparechange/containers/drawer/drawer.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,12 +22,108 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: MeSuiteColors.blue,
-        title: Text("SpareChange"),
-      ),
-      drawer: DrawerContainer(),
-      body: Text("Yooo"),
-    );
+        appBar: AppBar(
+          backgroundColor: MeSuiteColors.blue,
+          title: Text('Dashboard',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 25.0)),
+        ),
+        drawer: DrawerContainer(),
+        body: StaggeredGridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          children: <Widget>[
+            _buildTile(
+              Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Hello! Your SpareChange account value is:',
+                                style: TextStyle(
+                                    color: Colors.blueAccent, fontSize: 10.0)),
+                            Text('\$18.75',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 28.0))
+                          ],
+                        ),
+                        Material(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(24.0),
+                            child: Center(
+                                child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Icon(Icons.timeline,
+                                  color: Colors.white, size: 30.0),
+                            )))
+                      ])),
+            ),
+            _buildTile(
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Shop Items',
+                              style: TextStyle(color: Colors.redAccent)),
+                          Text('173',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 34.0))
+                        ],
+                      ),
+                      Material(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(24.0),
+                          child: Center(
+                              child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Icon(Icons.store,
+                                color: Colors.white, size: 30.0),
+                          )))
+                    ]),
+              ),
+            )
+          ],
+          staggeredTiles: [
+            StaggeredTile.extent(2, 110.0),
+            StaggeredTile.extent(2, 110.0),
+            StaggeredTile.extent(1, 180.0),
+            StaggeredTile.extent(2, 220.0),
+            StaggeredTile.extent(2, 110.0),
+          ],
+        ));
+  }
+
+  Widget _buildTile(Widget child, {Function() onTap}) {
+    return Material(
+        elevation: 14.0,
+        borderRadius: BorderRadius.circular(12.0),
+        shadowColor: Color(0x802196F3),
+        child: InkWell(
+            // Do onTap() if it isn't null, otherwise do print()
+            onTap: onTap != null
+                ? () => onTap()
+                : () {
+                    print('Not set yet');
+                  },
+            child: child));
   }
 }
